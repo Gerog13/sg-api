6e65f2016aba5ed480f9ddd991a28bf1a525ccfd
Gerónimo García <gerog13@hotmail.com>
2024-10-10 17:13:47 -0300

    add serverless http to deploy on vercel

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
 
   app.enableCors({
     origin: '*',
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
-
-  await app.listen(3030);
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(expressApp);
